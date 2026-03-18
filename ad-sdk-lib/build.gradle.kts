7516e607770c64b54723108b4009f6d3c705855c
Krish-tech08 <krischaurasia890@gmail.com>
2026-03-18 21:31:20 +0530

    fixed gradle

--- ad-sdk-lib/build.gradle.kts
+++ ad-sdk-lib/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.ksp)
+    id("maven-publish")
 }
 
 android {
@@ -30,6 +31,25 @@ android {
 
     kotlin {
         jvmToolchain(17)
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.Krish-tech08"
+                artifactId = "Dynamic_Ad_Library"
+                version = "1.0.0"
+            }
+        }
     }
 }
 
